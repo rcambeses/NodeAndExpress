89bfe318f19106e35c06a93811155cf73969ec5d
Ramon Cambeses <ramon@farodemarte.com>
2016-04-14 15:59:07 -0500

    zombie
    cross page tests with zombie
    not working

--- site/qa/tests-crosspage.js
+++ site/qa/tests-crosspage.js
@@ -10,7 +10,7 @@ suite('Cross-Page Tests', function(){
     });
 
     test('requesting a group rate quote from the hood river tour page' +
-        'should populate the referrer field', function(done){
+        ' should populate the referrer field', function(done){
         var referrer = 'http://localhost:3000/tours/hood-river';
         browser.visit(referrer, function(){
             browser.clickLink('.requestGroupRate', function(){
@@ -22,7 +22,7 @@ suite('Cross-Page Tests', function(){
     });
 
     test('requesting a group rate from the oregon coast tour page should ' +
-        'populate the referrer field', function(done){
+        ' populate the referrer field', function(done){
         var referrer = 'http://localhost:3000/tours/oregon-coast';
         browser.visit(referrer, function(){
             browser.clickLink('.requestGroupRate', function(){
@@ -34,7 +34,7 @@ suite('Cross-Page Tests', function(){
     });
 
     test('visiting the "request group rate" page directly should result ' +
-        'in an empty referrer field', function(done){
+        ' in an empty referrer field', function(done){
         browser.visit('http://localhost:3000/tours/request-group-rate',
             function(){
                 assert(browser.field('referrer').value === '');
